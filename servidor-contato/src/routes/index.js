8eb1df23674e7168dba11f01ae5cb95aaa46e58f
camilaveras <camilaverass@icloud.com>
2019-11-06 19:51:56 -0300

    alterações

--- servidor-contato/src/routes/index.js
+++ servidor-contato/src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express") //chamando o express
 const router = express.Router() // chamado o router
 
+
 router.get("/", function (req, res) {
   res.status(200).send({
     title: "Reprograma - Lista de contatos",
