2d9a0292f7a678834e9d681dd0d0e9faffc16c44
camilaverass <camilaverass@icloud.com>
2019-11-05 22:04:12 -0300

    alterações e inclusão do post - api

--- servidor-contato/src/controller/contatosController.js
+++ servidor-contato/src/controller/contatosController.js
@@ -1,18 +1,22 @@
-const contatos = require("../model/contatos");
+const model = require("../model/contatos"); //to na pasta model e chamo o arquivo contatos
 
 const getAll = (request, response) => {
   console.log(request.url);
-  response.status(200).send(contatos.model);
+  response.status(200).send(model.agenda);
 };
 
-const getById = (request, response) => {
-  const id = request.params.id;
-  response.status(200).send(contatos.find(tarefa => tarefa.id == id));
-};
+//const getById = (request, response) => {
+ // const id = request.params.id;
+ // response.status(200).send(contatos.find(tarefa => tarefa.id == id));
+//};
 
+const add = (request, response ) => {
+ model.agenda.contatos.push(request.body)  //serve para colocar algo dentro do contatos.js
+response.status(200).send()
+}
 
 module.exports = {
   getAll,
-  getById
+  //getById
+  add
 }
-
